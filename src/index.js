fb17816b36b955bd7669cd455606c0c5c2ebcad7
Maxim Dmitriev <maximv.dmitriev@gmail.com>
2019-07-03 17:39:16 +0300

    for ie11

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app/app';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+import "core-js/es";
 
 ReactDOM.render(<App />, document.getElementById('root'));
